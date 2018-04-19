c276a62369a7f6ec2528da87d3cf5d58efc22458
brunnoh26 <38539830+brunnoh26@users.noreply.github.com>
2018-04-19 16:01:51 -0300

    Add files via upload

---
+++ SensorTemperatura.ino
@@ -0,0 +1,52 @@
+
+#include <SPI.h>
+#include <LoRa.h>
+
+#include "DHT.h"
+
+#define DHTPIN A1 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+
+int counter = 0;
+// Conecte pino 1 do sensor (esquerda) ao +5V
+// Conecte pino 2 do sensor ao pino de dados definido em seu Arduino
+// Conecte pino 4 do sensor ao GND
+// Conecte o resistor de 10K entre pin 2 (dados) 
+// e ao pino 1 (VCC) do sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  dht.begin();
+}
+
+void loop() 
+{
+  // A leitura da temperatura e umidade pode levar 250ms!
+  // O atraso do sensor pode chegar a 2 segundos.
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+  else 
+  {
+    LoRa.beginPacket();
+    LoRa.print("Temperatura: ");
+    LoRa.print(t);
+    LoRa.print(" *C");
+    LoRa.endPacket();    
+  }
+  delay (1800000);
+}
